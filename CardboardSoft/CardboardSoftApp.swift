85623ed0bdc122064427546322ac7d86210d9c01
Goben Diego C.A <89054055+PocketNugget@users.noreply.github.com>
2024-02-27 15:18:42 -0600

    Add files via upload

---
+++ CardboardSoft/CardboardSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardboardSoftApp.swift
+//  CardboardSoft
+//
+//  Created by Nuggz on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CardboardSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardboardSoft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardboardSoft
+//
+//  Created by Nuggz on 27/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
