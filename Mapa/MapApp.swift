16e22a03bfcf442d952b32db508f7dbb73c1adbd
Andre Duhamel <116230414+DaWaildHorse@users.noreply.github.com>
2024-02-29 14:56:48 -0600

    Avances de Map

---
+++ Mapa/MapApp.swift
@@ -0,0 +1,18 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by iOS Lab on 27/02/24.
+//
+
+import SwiftUI
+import MapKit
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
