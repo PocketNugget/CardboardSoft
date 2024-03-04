9c96b49e7238bbb38883938b068ed6fe3f7f75da
Andre Duhamel <116230414+DaWaildHorse@users.noreply.github.com>
2024-03-04 11:49:47 -0600

    Map updates
    missing coordinates, working on in

---
+++ Mapa/MAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAPApp.swift
+//  MAP
+//
+//  Created by iOS Lab on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
